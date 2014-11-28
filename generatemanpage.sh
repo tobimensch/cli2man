4702c38624cab4baad06eb76190b7feb5370a7be
Tobias Gläßer <tobimensch@yahoo.de>
2014-11-28 22:32:36 +0100

    add self-generated manpage to repository

--- generatemanpage.sh
+++ generatemanpage.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-./cli2man ./cli2man | gzip > cli2man.1.gz
+./cli2man ./cli2man -o auto --gzip
 man2html -r ./cli2man.1.gz | sed 's/<I>\,\([^<]*\)/<I>\1replaceme/' | sed 's/\/replaceme//' > cli2man_manpage.html
