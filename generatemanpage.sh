f5adfbbb0c867e2a506cde62f1ccd9029b7caf17
Tobias Gläßer <tobimensch@yahoo.de>
2014-12-01 12:44:07 +0100

    man2html is obsolete

--- generatemanpage.sh
+++ generatemanpage.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 ./cli2man ./cli2man -o auto --gzip
-man2html -r ./cli2man.1.gz | sed 's/<I>\,\([^<]*\)/<I>\1replaceme/' | sed 's/\/replaceme//' > cli2man_manpage.html
+cat cli2man.1.gz | gunzip | mandoc -Thtml > cli2man_manpage.html
+
