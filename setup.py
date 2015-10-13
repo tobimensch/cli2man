c95af5def543e2703070a058215c0194c069b210
Benjamin Bach <benjaoming@gmail.com>
2015-10-13 18:48:19 +0200

    add docopt requirement

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ import sys, os, shutil
 from distutils.core import setup
 
 setup(name='cli2man',
-      version='0.2',
+      version='0.2.1',
       description='Converts the help message of a program into a manpage',
+      install_requires=['docopt'],
       author='Tobias Glaesser',
       url='https://github.com/tobimensch/cli2man',
       scripts=['cli2man'],
